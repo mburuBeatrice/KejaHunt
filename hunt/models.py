cc5972e2a93d50e522c640941743df74b4e6644a
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-03-21 15:36:45 +0300

    create category and house model and makes migrations

--- hunt/models.py
+++ hunt/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
-3
+
 
 # Create your models here.
+
+class Category(models.Model):
+    category = models.CharField(max_length=30)
+    
+class House(models.Model):
+    Location = models.CharField(max_length=30)
+    Image = models.ImageField()
+    Description = models.TextField(max_length=250)
+    Type = models.CharField(max_length=30)
+    category = models.ForeignKey(Category)
