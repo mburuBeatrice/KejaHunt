c9cfcd9bc4a10e7e0057363be4f68da0d69ea875
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-03-21 15:40:43 +0300

    create a contact model and makes migrations

--- hunt/models.py
+++ hunt/models.py
@@ -7,10 +7,26 @@ from tinymce.models import HTMLField
 
 class Category(models.Model):
     category = models.CharField(max_length=30)
-    
+
+
+    def __str__(self):
+        return self.category
+
 class House(models.Model):
     Location = models.CharField(max_length=30)
     Image = models.ImageField()
     Description = models.TextField(max_length=250)
     Type = models.CharField(max_length=30)
     category = models.ForeignKey(Category)
+
+
+    def __str__(self):
+        return self.Location
+
+class Contacts(models.Model):
+    name = models.CharField(max_length=30)
+    phone_number = models.IntegerField()
+    email_address = models.EmailField()
+
+    def __str__(self):
+        return self.name
