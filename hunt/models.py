1284e7cee2cf297dca2bd7173a4c2acc3d5158cb
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-03-23 07:57:31 +0300

    add changes to contact page

--- hunt/models.py
+++ hunt/models.py
@@ -15,7 +15,7 @@ class Category(models.Model):
 class House(models.Model):
     Location = models.CharField(max_length=30)
     Image = models.ImageField()
-    Description = models.TextField(max_length=250)
+    Description = models.TextField(max_length=1000)
     Type = models.CharField(max_length=30)
     contact_email = models.EmailField(default=1)
     contact_number = models.IntegerField(default=1)
